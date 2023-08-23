5b5ce88ed494698901b2c3a88c59c1252880a2dc
Mastagoon <inzilagiway@gmail.com>
2023-08-23 16:03:37 +0300

    diesel

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,27 @@
-use actix_web::Responder;
+use crate::models::User;
 
-pub async fn get_users() -> impl Responder {
-    format!("Hi mom")
+use super::schema::users::dsl::*;
+use super::Pool;
+use actix_web::{web, Error, HttpResponse};
+use diesel::RunQueryDsl;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InputUser {
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+}
+
+pub async fn get_users(db: web::Data<Pool>) -> Result<HttpResponse, Error> {
+    Ok(web::block(move || get_all_users(db))
+        .await
+        .map(|user| HttpResponse::Ok().json(user))
+        .map_err(|_| HttpResponse::InternalServerError())?)
+}
+
+fn get_all_users(pool: web::Data<Pool>) -> Result<Vec<User>, diesel::result::Error> {
+    let conn = pool.get().unwrap();
+    let items = users.load::<User>(&conn)?;
+    Ok(items)
 }
